ce89501ed53d60ba20a5fa9189a69dd6b95425c9
Andrew Hernandez <andrewh717@gmail.com>
2017-09-12 22:17:10 -0400

    Fixed scroll links for mobile
    Now checks for a CSS media query change in init.js to change scroll position when on mobile

--- js/init.js
+++ js/init.js
@@ -1,61 +1,93 @@
 $(document).ready(function () {
     $('#about-link').click(function () {
         var about = $('#about').position();
-        $('html, body').animate({
-            scrollTop: about.top
-        }, 500);
+        if($(".flow-text").css("padding-left") == "10px") {
+            $('html, body').animate({
+                scrollTop: about.top - 40
+            }, 500);
+        } else {
+            $('html, body').animate({
+                scrollTop: about.top
+            }, 500);
+        }
         sideNav().removeMenu();
         return false;
     });
 
     $('#education-link').click(function () {
         var education = $('#education').position();
-        $('html, body').animate({
-            scrollTop: education.top
-        }, 500);
+        if($(".flow-text").css("padding-left") == "10px") {
+            $('html, body').animate({
+                scrollTop: education.top - 40
+            }, 500);
+        } else {
+            $('html, body').animate({
+                scrollTop: education.top
+            }, 500);
+        }
         sideNav().removeMenu();
         return false;
     });
 
     $('#projects-link').click(function () {
         var projects = $('#projects').position();
-        $('html, body').animate({
-            scrollTop: projects.top
-        }, 500);
+        if($(".flow-text").css("padding-left") == "10px") {
+            $('html, body').animate({
+                scrollTop: projects.top - 40
+            }, 500);
+        } else {
+            $('html, body').animate({
+                scrollTop: projects.top
+            }, 500);
+        }
         sideNav().removeMenu();
         return false;
     });
 
     $('#experience-link').click(function () {
         var experience = $('#experience').position();
-        $('html, body').animate({
-            scrollTop: experience.top
-        }, 500);
+        if($(".flow-text").css("padding-left") == "10px") {
+            $('html, body').animate({
+                scrollTop: experience.top - 40
+            }, 500);
+        } else {
+            $('html, body').animate({
+                scrollTop: experience.top
+            }, 500);
+        }
         sideNav().removeMenu();
         return false;
     });
 
     $('#contact-link1').click(function () {
         var contact = $('#contact').position();
-        $('html, body').animate({
-            scrollTop: contact.top
-        }, 500);
+        if($(".flow-text").css("padding-left") == "10px") {
+            $('html, body').animate({
+                scrollTop: contact.top - 40
+            }, 500);
+        } else {
+            $('html, body').animate({
+                scrollTop: contact.top
+            }, 500);
+        }
         sideNav().removeMenu();
         return false;
     });
 
     $('#contact-link2').click(function () {
         var contact = $('#contact').position();
-        $('html, body').animate({
-            scrollTop: contact.top
-        }, 500);
+        if($(".flow-text").css("padding-left") == "10px") {
+            $('html, body').animate({
+                scrollTop: contact.top - 40
+            }, 500);
+        } else {
+            $('html, body').animate({
+                scrollTop: contact.top
+            }, 500);
+        }
         sideNav().removeMenu();
         return false;
     });
-});
-
-$("ul#slide-out li a").click(function () {
-    
 });
 
 $(".button-collapse").sideNav();
