0c709244e06956dd4281062869eaea6b4183239c
Andrew H <andrewh717@gmail.com>
2021-11-01 16:24:00 -0400

    React rebuild

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, my name is <span className="blue">Andrew Hernandez</span></h1>
+      <p>I'm a software engineer currently working at AT&amp;T in the Automation Platform Development organization.</p>
+      <div className="links">
+        <a href="assets/resume.pdf" target="_blank" rel="noreferrer noopener">Resume</a>
+        <a href="https://github.com/andrewh717" target="_blank" rel="noreferrer noopener">Github</a>
+        <a href="https://www.linkedin.com/in/andrewh717/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
