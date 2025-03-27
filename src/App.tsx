15971dd400645810866bda958062c0dcaa0bbbc8
Andrew H. <andrewh717@gmail.com>
2025-03-26 23:24:31 -0400

    Updating for 2025

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, my name is <span className="blue">Andrew Hernandez</span></h1>
-      <p>I'm a software engineer currently working at AT&amp;T in the Automation Platform Development organization.</p>
+      <p>I'm a software engineer currently working at AT&amp;T in the Software Delivery Services organization.</p>
       <div className="links">
-        <a href="assets/resume.pdf" target="_blank" rel="noreferrer noopener">Resume</a>
+        <a href="assets/resume_2025.pdf" target="_blank" rel="noreferrer noopener">Resume</a>
         <a href="https://github.com/andrewh717" target="_blank" rel="noreferrer noopener">Github</a>
         <a href="https://www.linkedin.com/in/andrewh717/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
       </div>
