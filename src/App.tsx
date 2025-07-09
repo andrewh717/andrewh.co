b04561f86f81493f2e813737a344218bc01926aa
Andrew H. <andrewh717@gmail.com>
2025-07-08 21:28:27 -0400

    Updating resume

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <h1>Hi, my name is <span className="blue">Andrew Hernandez</span></h1>
       <p>I'm a software engineer currently working at AT&amp;T in the Software Delivery Services organization.</p>
       <div className="links">
-        <a href="assets/resume_2025.pdf" target="_blank" rel="noreferrer noopener">Resume</a>
+        <a href="assets/Andrew_Hernandez_Resume.pdf" target="_blank" rel="noreferrer noopener">Resume</a>
         <a href="https://github.com/andrewh717" target="_blank" rel="noreferrer noopener">Github</a>
         <a href="https://www.linkedin.com/in/andrewh717/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
       </div>
